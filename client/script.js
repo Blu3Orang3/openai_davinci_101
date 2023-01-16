f495c54addd92623ee4a31e458d33df3642cedc5
BlueOrang3 <matealtunashvili@gmial.com>
2023-01-17 02:10:15 +0400

    server endpoint set to .render.

--- client/script.js
+++ client/script.js
@@ -88,15 +88,15 @@ const handleSubmit = async (e) => {
   clearInterval(loadInterval);
   messageDiv.innerHTML = '';
 
-  if(response.ok) {
-    const data = await response.json()
-    const parsedData = data.bot.trim()
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
 
-    typeText(messageDiv, parsedData)
+    typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
 
-    messageDiv.innerHTML = "Something went wrong";
+    messageDiv.innerHTML = 'Something went wrong';
 
     alert(err);
   }
